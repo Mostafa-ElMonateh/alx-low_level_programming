bc120cbf55728c26534f50ce2cd2546f42f9b96a
Mostafa ElMonateh <ElMonateh@gmail.com>
2022-12-08 10:14:44 -0800

    Runs a C file through the preprocessor and save the result into another file

---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/* main - Entry point
+ *
+ * Description: 'Print Hello World'
+ *
+ * Return : Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("Hello World\n")
+	return (0);
+}
