6e1e0f594ed318ca1cb1831366338efcba882031
Mostafa ElMonateh <ElMonateh@gmail.com>
2022-12-15 03:39:08 -0800

    editting betty style

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,9 @@
 #include "main.h"
 
 /**
- * print_most_numbers - a function that prints the numbers, form 0 to 9 followed by a new line
+ * print_most_numbers - a function that prints the numbers, form 0 to 9
+ * not 2 and 4
+ * followed by a new line
  *
  * Return: void
  */
