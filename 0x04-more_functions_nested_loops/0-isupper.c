a48baa03a5a5f4b7ff72bc976fbba36989ada5b9
Mostafa ElMonateh <ElMonateh@gmail.com>
2022-12-15 03:04:12 -0800

    adding a description

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  * _isupper() - a function that checks for uppercase character.
+ * Description: checking for uppercase character.
  * @c: input.
  * Return: 1 if c is uppercase
  * 0 otherwise.
