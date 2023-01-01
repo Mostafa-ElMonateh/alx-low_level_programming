e0389306358408e50a9c21d78fd1a5ad48b1e8de
Mostafa ElMonateh <108753275+Mostafa-ElMonateh@users.noreply.github.com>
2023-01-01 15:47:51 +0200

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,8 +4,7 @@
  * print_rev - prints a string in reverse
  * @s: string
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: Void
  */
 void print_rev(char *s)
 {
