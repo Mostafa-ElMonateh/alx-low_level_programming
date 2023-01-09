c0db0eda464316db31b486eeeb90eb6b14b7bb79
Mostafa ElMonateh <ElMonateh@gmail.com>
2023-01-09 09:13:20 -0800

    2-str_concat.c has a function that concatenates two strings.

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,6 +2,38 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
+ * _strlen - return length of a string
+ *
+ * @s: char type
+ * Return:  length of string
+ */
+int _strlen(char *s)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+	;
+	return (a);
+}
+/**
+ * _strncat - function to concatnate strings with n bytes
+ *
+ * @dest: destination for concatnation
+ * @src: source of string
+ * @n: int type for size of byte
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_len, a;
+
+	dest_len = _strlen(dest);
+	for (a = 0; a < n && src[a] != '\0'; a++)
+		dest[dest_len + a] = src[a];
+	return (dest);
+}
+
+/**
  * *str_concat - function to allocate space for sting concatnation
  * @s1: array pointer to destination of string
  * @s2: array pointer to source of string
@@ -10,39 +42,27 @@
 
 char *str_concat(char *s1, char *s2)
 {
-    int index1, index2, i;
-    char *arr;
+	char *ptr;
+	int size1, size2;
 
-    if (s1 == NULL || s2 == NULL)
-        s1 = s2 = "";
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	size1 = _strlen(s1);
+	size2 = _strlen(s2);
 
-    while (s1[index1] != '\0')
-    {
-        index1++;
-    }
-
-    while (s2[index2] != '\0')
-    {
-        index2++;
-    }
-    index2++;
-
-    arr = malloc((index1 + index2 + 1) * sizeof(char));
-
-    while (*s1)
-    {
-        arr[i] = *s1;
-        i++;
-        s1++;
-    }
-
-    while (*s2)
-    {
-        arr[i] = *s2;
-        i++;
-        s2++;
-    }
-    arr[i] = *s2;
-
-    return (arr);
+	ptr = malloc(((size1 + size2) + 1) * sizeof(char));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	_strncat(ptr, s1, size1);
+	_strncat(ptr, s2, size2);
+	ptr += '\0';
+	return (ptr);
 }
